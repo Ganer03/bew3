33ab276ae9ab72cbcff0cd45003f95860e5d76a6
Ganer03 <112845740+Ganer03@users.noreply.github.com>
2023-03-13 08:45:55 +0300

    Update form.php

--- form.php
+++ form.php
@@ -1,5 +1,6 @@
 <form action="" method="POST">
   <input name="fio" />
+  <input enail="email" />
   <select name="year">
     <?php 
     for ($i = 1922; $i <= 2022; $i++) {
@@ -7,6 +8,51 @@
     }
     ?>
   </select>
-  
+  Пол:<br/>
+  <label>
+  <input type="radio" checked="checked" name="pol"/>
+    M
+  </label>
+  <label>
+  <input type="radio" checked="checked" name="pol"/>
+    W
+  </label>
+  Кол-во конечностей:<br/>
+        <label>
+            <input type="radio" checked="checked" name="radio-group-2"/>
+            1
+        </label>
+        <label>
+            <input type="radio" name="radio-group-2"/>
+            2
+        </label>
+        <label>
+            <input type="radio" name="radio-group-2"/>
+            3
+        </label>
+        <label>
+            <input type="radio" name="radio-group-2"/>
+            4
+        </label>
+        <label>
+            <input type="radio" name="radio-group-2"/>
+            5
+        </label><br/>
+    <label>
+            Сверхспособности:<br/>
+            <select name="field-name-4[]" multiple="multiple">
+                <option>бессмертие</option>
+                <option selected="selected">прохождение сквозь стены</option>
+                <option selected="selected">левитация</option>
+            </select>
+        </label><br/>
+  <label>
+            Биография:<br/>
+            <textarea name="field-name-2">Расскажите о себе</textarea>
+        </label><br/>
+          <label>
+            <input type="checkbox" checked="checked" name="check-1"/>
+            с контрактом ознакомлен (а)
+        </label><br/>
   <input type="submit" value="ok" />
 </form>
