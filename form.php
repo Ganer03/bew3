07c6e9f7fa27d64364389ce7ce15af80a8128480
Ganer03 <112845740+Ganer03@users.noreply.github.com>
2023-03-29 00:47:59 +0300

    Update form.php

--- form.php
+++ form.php
@@ -6,63 +6,63 @@
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
-<form action="" method="POST">
-  <input name="fio" />
-  <input enail="email" />
-  <select name="year">
-    <?php 
-    for ($i = 1922; $i <= 2022; $i++) {
-      printf('<option value="%d">%d год</option>', $i, $i);
-    }
-    ?>
-  </select>
-  Пол:<br/>
-  <label>
-  <input type="radio" checked="checked" name="pol"/>
-    M
-  </label>
-  <label>
-  <input type="radio" checked="checked" name="pol"/>
-    W
-  </label>
-  Кол-во конечностей:<br/>
-        <label>
-            <input type="radio" checked="checked" name="radio-group-2"/>
-            1
-        </label>
-        <label>
-            <input type="radio" name="radio-group-2"/>
-            2
-        </label>
-        <label>
-            <input type="radio" name="radio-group-2"/>
-            3
-        </label>
-        <label>
-            <input type="radio" name="radio-group-2"/>
-            4
-        </label>
-        <label>
-            <input type="radio" name="radio-group-2"/>
-            5
-        </label><br/>
+<form action="/index.php" method="POST">
+    <input name="fio" value="Введите ФИО"/>
+    <input name="email" value="Введите почту"/>
+    <select name="year">
+        <?php
+        for ($i = 1922; $i <= 2022; $i++) {
+            printf('<option value="%d">%d год</option>', $i, $i);
+        }
+        ?>
+    </select>
+    Пол:<br/>
     <label>
-            Сверхспособности:<br/>
-            <select name="field-name-4[]" multiple="multiple">
-                <option value='бессмертие'>бессмертие</option>
-                <option value='прохождение сквозь стены' selected="selected">прохождение сквозь стены</option>
-                <option value='левитация' selected="selected">левитация</option>
-            </select>
-        </label><br/>
-  <label>
-            Биография:<br/>
-            <textarea name="field-name-2">Расскажите о себе</textarea>
-        </label><br/>
-          <label>
-            <input type="checkbox" checked="checked" name="check-1"/>
-            с контрактом ознакомлен (а)
-        </label><br/>
-  <input type="submit" value="ok" />
+        <input type="radio" checked="checked" name="pol" value="M"/>
+        M
+    </label>
+    <label>
+        <input type="radio" name="pol" value="W"/>
+        W
+    </label>
+    Кол-во конечностей:<br/>
+    <label>
+        <input type="radio" checked="checked" name="limbs" value="1"/>
+        1
+    </label>
+    <label>
+        <input type="radio" name="limbs" value="2"/>
+        2
+    </label>
+    <label>
+        <input type="radio" name="limbs" value="3"/>
+        3
+    </label>
+    <label>
+        <input type="radio" name="limbs" value="4"/>
+        4
+    </label>
+    <label>
+        <input type="radio" name="limbs" value="5"/>
+        5
+    </label><br/>
+    <label>
+        Сверхспособности:<br/>
+        <select name="super[]" multiple="multiple">
+            <option value='1'>бессмертие</option>
+            <option value='2' selected="selected">прохождение сквозь стены</option>
+            <option value='3' selected="selected">левитация</option>
+        </select>
+    </label><br/>
+    <label>
+        Биография:<br/>
+        <textarea name="biography">Расскажите о себе</textarea>
+    </label><br/>
+    <label>
+        <input type="checkbox" checked="checked" name="check-1"/>
+        с контрактом ознакомлен (а)
+    </label><br/>
+    <input type="submit" value="Отправить" />
 </form>
-  </body>
+</body>
 </html>
